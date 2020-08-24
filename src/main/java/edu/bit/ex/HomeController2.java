b7db49968ed08c19679eab74b5d4c4e64de0a6ec
KUyejin <kooyj92@hanmail.net>
2020-08-24 10:50:32 +0900

    컨트롤러 추가

---
+++ src/main/java/edu/bit/ex/HomeController2.java
@@ -0,0 +1,45 @@
+package edu.bit.ex;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+//test4
+//test3
+//계속테스트
+//깃폴더에 가져온거 임포트
+//version2
+//version1
+@Controller
+public class HomeController2 {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController2.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
